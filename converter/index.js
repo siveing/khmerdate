7f45b5d43b9c9066d9f0f27e7456ced84498d935
Siveing <siveing.huyy@gmail.com>
2023-07-25 20:33:52 +0700

    added and update latest version

--- converter/index.js
+++ converter/index.js
@@ -1,23 +1,23 @@
-const { Config } = require("../config");
+const { Constant } = require("../constant");
 
 class DateConverter {
     constructor(date) {
         this.dateTime = date ? date : new Date();
-        this.config = new Config();
+        this.constant = new Constant();
     }
 
     formatL() {
         return `${this.day()}/${this.month()}/${this.year()}`;
     }
 
-    formatLL() {
-        return `${this.day()} ${this.fullMonth()} ${this.year()}`;
-    }
-
     formatT() {
         return `${this.hour()}:${this.minute()} ${this.shift()}`;
     }
 
+    formatLL() {
+        return `${this.day()} ${this.fullMonth()} ${this.year()}`;
+    }
+   
     formatLLL() {
         return `${this.fullDay()} ${this.day()} ${this.fullMonth()} ${this.year()}`;
     }
@@ -26,9 +26,63 @@ class DateConverter {
         return `ថ្ងៃ${this.fullDay()} ទី${this.day()} ខែ${this.fullMonth()} ឆ្នាំ${this.year()}`;
     }
 
+    day() {
+        return this.constant.getNumbers(new Date(this.dateTime).getDate());
+    }
+
+    fullDay() {
+        return this.constant.getDay(new Date(this.dateTime).getDay());
+    }
+
+    month() {
+        return this.constant.getMonth(new Date(this.dateTime).getMonth());
+    }
+
+    year() {
+        return this.constant.getNumbers(new Date(this.dateTime).getFullYear());
+    }
+
+    hour() {
+        let hour = new Date(this.dateTime).getHours();
+        return this.constant.getNumbers(hour);
+    }
+
+    minute() {
+        return this.constant.getNumbers(new Date(this.dateTime).getMinutes());
+    }
+
+    shift() {
+        let shiftVal = new Date(this.dateTime).getHours() >= 12 ? "PM" : "AM";
+        return this.constant.getShift(shiftVal);
+    }
+
+    week() {
+        let firstOfMonth = new Date(new Date(this.dateTime).getFullYear(), new Date(this.dateTime).getMonth(), 1);
+        let weekOfMonth = Math.ceil((new Date(this.dateTime).getDate() + firstOfMonth.getDay()) / 7);
+
+        return this.constant.getNumbers(weekOfMonth);
+    }
+
+    weekOfYear() {
+        return this.constant.numbers(new Date(this.dateTime).getWeek());
+    }
+
+    fullMonth(){
+        return this.constant.getMonth(new Date().getMonth() + 1);
+    }
+
+    format(format) {
+        try {
+            return this.dateTimeFormat(format);
+        } catch (Exception) {
+            throw new Exception("Invalid format");
+        }
+    }
+
     dateTimeFormat(format) {
         const formats = {
             'L': this.formatL(),
+            'LT':`${this.formatL()} ${this.formatT()}`,
             'LL': this.formatLL(),
             'LLT': `${this.formatLL()} ${this.formatT()}`,
             'LLL': this.formatLLL(),
@@ -40,106 +94,12 @@ class DateConverter {
         return formats[format];
     }
 
-    day() {
-        return this.config.getNumbers(new Date(this.dateTime).getDate());
-    }
-
-    fullDay() {
-        return this.config.getDay(new Date(this.dateTime).getDay());
-    }
-
-    month() {
-        return this.config.getMonth(new Date(this.dateTime).getMonth());
-    }
-
-    year() {
-        return this.config.getNumbers(new Date(this.dateTime).getFullYear());
-    }
-
-    hour() {
-        let hour = new Date(this.dateTime).getHours();
-        return this.config.getNumbers(hour);
-    }
-
-    minute() {
-        return this.config.getNumbers(new Date(this.dateTime).getMinutes());
-    }
-
-    quarter() {
-        let month = new Date(this.dateTime).getMonth() + 1;
-        return this.config.getNumbers(Math.ceil(month / 3));
-    }
-
-    fullQuarter() {
-        return "ត្រីមាសទី" + this.quarter();
-    }
-
-    shift() {
-        let shiftVal = new Date(this.dateTime).getHours() >= 12 ? "PM" : "AM";
-        return this.config.getShift(shiftVal);
-    }
-
-    week() {
-        let firstOfMonth = new Date(new Date(this.dateTime).getFullYear(), new Date(this.dateTime).getMonth(), 1);
-        let weekOfMonth = Math.ceil((new Date(this.dateTime).getDate() + firstOfMonth.getDay()) / 7);
-
-        return this.config.getNumbers(weekOfMonth);
-    }
-
     fullWeek() {
         return "សប្តាហ៍ទី" + this.week();
     }
 
-    weekOfYear() {
-        return this.config.numbers(new Date(this.dateTime).getWeek());
-    }
-
     fullWeekOfYear() {
         return "សប្តាហ៍ទី" + this.weekOfYear();
-    }
-
-    fullMonth(){
-        return this.config.getMonth(new Date().getMonth() + 1);
-    }
-
-    format(format) {
-        try {
-            return this.dateTimeFormat(format);
-        } catch (Exception) {
-            throw new Exception("Invalid format");
-        }
-    }
-
-    fromNow(space = true) {
-        return this.durationFrom(new Date(), space);
-    }
-
-    durationFrom(now, space) {
-        let interval = this.toDateTimeFormat().diff(now);
-        let suffix = now > this.toDateTimeFormat() ? 'មុន' : 'ទៀត';
-        let durationSpace = space ? ' ' : '';
-
-        if (interval.y >= 1) {
-            return this.config.getNumbers(interval.y) + durationSpace + 'ឆ្នាំ' + suffix;
-        }
-        if (interval.m >= 1) {
-            return this.config.getNumbers(interval.m) + durationSpace + 'ខែ' + suffix;
-        }
-        if (interval.d >= 1) {
-            return this.config.getNumbers(interval.d) + durationSpace + 'ថ្ងៃ' + suffix;
-        }
-        if (interval.h >= 1) {
-            return this.config.getNumbers(interval.h) + durationSpace + 'ម៉ោង' + suffix;
-        }
-        if (interval.i >= 1) {
-            return this.config.getNumbers(interval.i) + durationSpace + 'នាទី' + suffix;
-        }
-
-        return this.config.getNumbers(interval.s) + durationSpace + 'វិនាទី' + suffix;
-    }
-
-    toDateTimeFormat() {
-        return new Date(this.dateTime);
     }
 }
 
